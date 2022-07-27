466e9167aab750a20fdd706c45831c7c0cf0e7ad
kirill <skivpost@gmail.com>
2022-07-27 14:07:33 +0300

    22

--- problems/022-parentheses.js
+++ problems/022-parentheses.js
@@ -16,7 +16,24 @@
  * @returns {boolean}
  */
 function parentheses(value) {
-    return undefined;
+    let closeIsLast=false
+    let openCounter=0
+    let closeCounter=0
+    for(ch of value){
+        if(ch===')' && openCounter===0){
+            return false
+        }
+        else if(ch==='('){
+            openCounter++
+            closeIsLast=false
+        }
+        else if(ch===')' && openCounter!=0){
+            closeCounter++
+            closeIsLast=true
+        }
+    }
+    return closeIsLast && openCounter===closeCounter ? true: false;
 }
+
 
 module.exports = parentheses;
