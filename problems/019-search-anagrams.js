f611f92aa27ced656e179408c89d9715462fad4d
kirill <skivpost@gmail.com>
2022-07-27 03:29:00 +0300

    19

--- problems/019-search-anagrams.js
+++ problems/019-search-anagrams.js
@@ -12,7 +12,23 @@
  * @returns {string}
  */
 function searchAnagrams(value) {
-    return undefined;
+    const res=[]
+    value= value.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    const arr=value.split(' ')
+    console.log(arr)
+
+    for(i=0;i<arr.length;i++){
+        for(j=0;j<arr.length;j++){
+            let first=arr[i].toUpperCase().split("").sort().join()
+            let  second=arr[j].toUpperCase().split("").sort().join()
+    
+            if (first===second  && arr[i]&& arr[j]&& arr[i]!=arr[j]) {
+               res.push(arr[i])
+            }
+         continue
+        }
+    }   
+   return res.join(' ')
 }
 
 module.exports = searchAnagrams;
