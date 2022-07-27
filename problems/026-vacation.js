0acdbd7b021b6e0a764cd971e4adf7c6988344be
kirill <skivpost@gmail.com>
2022-07-27 17:08:35 +0300

    26

--- problems/026-vacation.js
+++ problems/026-vacation.js
@@ -14,7 +14,25 @@
  * @returns {string}
  */
 function vacation(date) {
-    return undefined;
+    const res = [];
+    date = date.split(".").reverse().join("-");
+    console.log(date);
+    firstDate = new Date(Date.parse(date));
+    console.log(firstDate);
+    resDate = new Date(firstDate.setDate(firstDate.getDate() + 14));
+    if (resDate.getDate() < 10) {
+        res.push("0" + resDate.getDate());
+    } else {
+        res.push(resDate.getDate());
+    }
+
+    if (resDate.getMonth() + 1 < 10) {
+        res.push("0" + (resDate.getMonth() + 1));
+    } else {
+        res.push(resDate.getMonth() + 1);
+    }
+    res.push(resDate.getFullYear());
+    return res.join(".");
 }
 
 module.exports = vacation;
